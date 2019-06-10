827b11e227b091c5b0861f41e81d8bfd49ecd417
iOSweepingMonk <740305122@qq.com>
2019-06-10 14:45:36 +0800

    init library

---
+++ Example/Pods/Target Support Files/TMColorTools/TMColorTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TMColorTools : NSObject
+@end
+@implementation PodsDummy_TMColorTools
+@end
---
+++ Example/Pods/Target Support Files/TMColorTools/TMColorTools-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UIColor+HexColor.h"
+
+FOUNDATION_EXPORT double TMColorToolsVersionNumber;
+FOUNDATION_EXPORT const unsigned char TMColorToolsVersionString[];
+
